6d0e11b818a5d7762a902b7bc24abffec4cdf895
Thomas Dickson <modscrat@gmail.com>
2023-02-15 11:11:22 +1300

    Added Favicon

--- conf.py
+++ conf.py
@@ -7,14 +7,15 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'GCEC Wiki'
-copyright = '2023, GCEC Wiki Authors'
+copyright = '2023, GCEC Wiki Authors. GCEC Griffin © Copyright Gary Fern'
 author = 'GCEC Members'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx_rtd_theme'
+    'sphinx_rtd_theme',
+    'sphinx-favicon'
 ]
 
 templates_path = ['_templates']
@@ -29,8 +30,19 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_style = 'css/custom-theme.css'
 html_logo = 'logo.png'
-html_favicon = 'favicon.ico'
 
 html_theme_options = {
     'style_nav_header_background': '#ff9100'
 }
+
+favicons = [
+    {"href": "_static/favicon/android-chrome-192x192.png"},
+    {"href": "_static/favicon/android-chrome-512x512.png"},
+    {"href": "_static/favicon/favicon-16x16.png"},
+    {"href": "_static/favicon/favicon-32x32.png"},
+    {"href": "_static/favicon/favicon.ico"},
+    {
+        "rel": "apple-touch-icon",
+        "href": "_static/favicon/apple-touch-icon.png",
+    }
+]
